07662e32aae12ef4ac7c78b23bdcf1c35c4b9b96
Chamberlain <chamberlain@localhost.localdomain>
2025-02-18 10:16:17 +0100

    fix navbar interaction on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,23 +10,11 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="bg-gray-800 p-4 text-white">
+        <nav className="bg-gray-800 p-4 text-white relative">
             <div className="container mx-auto flex items-center justify-between">
-                {/* Logo et nom à gauche */}
-                <div className="flex items-center">
-                    <img src="/logo.png" alt="Logo" className="h-8 w-auto mr-4" />
-                    <span className="font-bold">Pokedex</span>
-                </div>
-
-                {/* Texte au centre (version mobile) */}
-                <div className="md:hidden text-center">
-                    <div>Fait par 0xChamberlain</div>
-                    <div>/pw Citadel</div>
-                </div>
-
-                {/* Menu burger à droite */}
-                <div className="md:hidden">
-                    <button onClick={toggleMenu} className="focus:outline-none">
+                {/* Menu burger à gauche (version mobile) */}
+                <div className={`md:hidden ${isOpen ? 'order-first' : ''}`}>
+                    <button onClick={toggleMenu} className="focus:outline-none z-30">
                         <svg
                             className="h-6 w-6"
                             fill="none"
@@ -44,20 +32,28 @@ export default function Navbar() {
                     </button>
                 </div>
 
-                {/* Liens de navigation (version desktop) */}
-                <div className="hidden md:flex space-x-4">
-                    <Link href="/" className="hover:text-gray-300 flex items-center">
-                        <img src="/icons/pokedex.svg" alt="Pokédex Icon" className="h-5 w-5 mr-2" />
-                        Pokédex
-                    </Link>
-                    <Link href="/wishlist" className="hover:text-gray-300 flex items-center">
-                        <img src="/icons/wishlist.svg" alt="Wishlist Icon" className="h-5 w-5 mr-2" />
-                        Wishlist
-                    </Link>
+                {/* Logo et nom (version mobile) */}
+                <div className={`md:hidden text-center ${isOpen ? 'hidden' : ''}`}>
+                    <img src="/logo.png" alt="Logo" className="h-12 w-auto mr-1 inline-block"/>
+                    <div className="flex flex-col items-center">
+                        <span className="font-bold">Citadel</span>
+                        <span className="font-bold">PokeWishlist</span>
+                    </div>
                 </div>
 
+                {/* Texte au centre (version mobile) */}
+                <div
+                    className={`md:hidden text-center absolute left-1/2 transform -translate-x-1/2 ${isOpen ? 'hidden' : ''}`}>
+                    <div>Fait par 0xChamberlain</div>
+                    <div>/pw Citadel</div>
+                </div>
+
+                {/* Masque de la navbar (version mobile) */}
+                <div className={`md:hidden absolute top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 z-10 ${isOpen ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}
+                     onClick={toggleMenu}></div>
+
                 {/* Liens de navigation (version mobile) */}
-                <div className={`md:hidden absolute top-full left-0 w-full bg-gray-800 z-10 ${isOpen ? 'block' : 'hidden'}`}>
+                <div className={`md:hidden absolute top-full left-0 w-full bg-gray-800 z-20 ${isOpen ? 'block' : 'hidden'}`}>
                     <div className="flex flex-col items-center p-4">
                         <Link href="/" className="hover:text-gray-300 flex items-center mb-2">
                             <img src="/icons/pokedex.svg" alt="Pokédex Icon" className="h-5 w-5 mr-2" />
@@ -70,8 +66,29 @@ export default function Navbar() {
                     </div>
                 </div>
 
+                {/* Logo et nom (version desktop) */}
+                <div className="hidden md:flex items-center">
+                    <img src="/logo.png" alt="Logo" className="h-14 w-auto mr-4" />
+                    <div className="flex flex-col items-center">
+                        <span className="font-bold">Citadel</span>
+                        <span className="font-bold">PokeWishlist</span>
+                    </div>
+                </div>
+
+                {/* Liens de navigation (version desktop) */}
+                <div className="hidden md:flex justify-center flex-grow space-x-4">
+                    <Link href="/" className="hover:text-gray-300 flex items-center">
+                        <img src="/icons/pokedex.svg" alt="Pokédex Icon" className="h-5 w-5 mr-2" />
+                        Pokédex
+                    </Link>
+                    <Link href="/wishlist" className="hover:text-gray-300 flex items-center">
+                        <img src="/icons/wishlist.svg" alt="Wishlist Icon" className="h-5 w-5 mr-2" />
+                        Wishlist
+                    </Link>
+                </div>
+
                 {/* Texte à droite (version desktop) */}
-                <div className="hidden md:flex text-right">
+                <div className="hidden md:flex flex-col items-end">
                     <div>Fait par 0xChamberlain</div>
                     <div>/pw Citadel</div>
                 </div>
