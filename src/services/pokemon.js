ee3dfd1a2be87688f63445bffcbc4181ab280363
Thomas Gauthier <ek0onsec@tuta.io>
2025-02-21 10:06:03 +0100

    add limit to 100000 pokemon ??

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,5 +1,5 @@
 export const fetchPokemonList = async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000');
     const data = await response.json();
 
     // Récupérer les détails de chaque Pokémon (nom et types en français)
